3fa53413bf887d3ded0a3f32f52f28f9cedfd694
Malthe <malthegram22@gmail.com>
2023-10-24 10:44:17 +0200

    models and data

--- ddl.js
+++ ddl.js
@@ -1,9 +1,11 @@
-import { Sequelize, DataTypes } from "sequelize";
+import models from "./models.js";
+import {Sequelize} from "sequelize";  // Adjust this import to the path where your models are located
 
 const sequelize = new Sequelize('sql_store', 'admin1', 'Password123', {
     host: 'sql-store-kea-sd23.mysql.database.azure.com',
     dialect: 'mysql',
     port: 3306,
+    logging: console.log,  // Enable this line
     dialectOptions: {
         ssl: {
             require: true,
@@ -12,194 +14,96 @@ const sequelize = new Sequelize('sql_store', 'admin1', 'Password123', {
     }
 });
 
-// Models
-const Category = sequelize.define('Category', {
-    name: {
-        type: DataTypes.STRING,
-        allowNull: true
-    }
-});
 
-const OrderStatus = sequelize.define('OrderStatus', {
-    name: {
-        type: DataTypes.ENUM('Order received', 'In Progress', 'Order delivered'),
-        allowNull: true
-    }
-});
-
-const Product = sequelize.define('Product', {
-    name: {
-        type: DataTypes.STRING,
-        allowNull: true
-    },
-    description: {
-        type: DataTypes.STRING,
-        allowNull: true
-    },
-    price: {
-        type: DataTypes.DECIMAL,
-        allowNull: true
-    },
-    imageurl: {
-        type: DataTypes.STRING,
-        allowNull: true
-    }
-});
-
-const Inventory = sequelize.define('Inventory', {
-    stock: {
-        type: DataTypes.SMALLINT,
-        allowNull: true
-    }
-});
-
-const User = sequelize.define('User', {
-    firstName: DataTypes.STRING,
-    lastName: DataTypes.STRING,
-    password: DataTypes.STRING,
-    email: DataTypes.STRING,
-    phone_number: DataTypes.STRING
-});
-
-const Address = sequelize.define('Address', {
-    zip_code: DataTypes.STRING,
-    street_name: DataTypes.STRING,
-    street_number: DataTypes.STRING,
-    city: DataTypes.STRING
-});
-
-const Payment = sequelize.define('Payment', {
-    total_price: DataTypes.DECIMAL
-});
-
-const Order = sequelize.define('Order', {
-    date: DataTypes.DATE
-});
-
-const OrderLineItem = sequelize.define('OrderLineItem', {});
-
-const PaymentDetail = sequelize.define('PaymentDetail', {
-    card_number: DataTypes.STRING,
-    transaction_number: DataTypes.STRING
-});
-
-// Dummy Data
 async function insertDummyData() {
-    await sequelize.sync();
+    // Inserting Category
+    const category1 = await models.Category.create({ name: 'Electronics' });
+    const category2 = await models.Category.create({ name: 'Clothing' });
+    const category3 = await models.Category.create({ name: 'Toys' });
 
-    const categories = await Category.bulkCreate([
-        { name: 'Electronics' },
-        { name: 'Clothing' },
-        { name: 'Toys' }
-    ]);
+    // Inserting Products
+    const product1 = await models.Product.create({
+        name: 'Laptop',
+        description: 'Gaming laptop',
+        price: 1500,
+        imageurl: 'https://example.com/laptop.png',
+        categoryId: category1.id
+    });
 
-    const orderStatuses = await OrderStatus.bulkCreate([
-        { name: 'Order received' },
-        { name: 'In Progress' },
-        { name: 'Order delivered' }
-    ]);
+    const product2 = await models.Product.create({
+        name: 'Shirt',
+        description: 'Cotton shirt',
+        price: 25,
+        imageurl: 'https://example.com/shirt.png',
+        categoryId: category2.id
+    });
 
-    const users = await User.bulkCreate([
-        { firstName: 'John', lastName: 'Doe', email: 'john.doe@example.com', password: 'john123', phone_number: '1234567890' },
-        { firstName: 'Jane', lastName: 'Doe', email: 'jane.doe@example.com', password: 'jane123', phone_number: '0987654321' },
-        { firstName: 'Bob', lastName: 'Smith', email: 'bob.smith@example.com', password: 'bob123', phone_number: '1122334455' }
-    ]);
+    const product3 = await models.Product.create({
+        name: 'Action Figure',
+        description: 'Superhero action figure',
+        price: 15,
+        imageurl: 'https://example.com/toy.png',
+        categoryId: category3.id
+    });
 
-    const products = await Product.bulkCreate([
-        { name: 'Laptop', description: 'Gaming laptop', price: 1500.50, categoryId: categories[0].id, imageurl: "https://miro.medium.com/v2/resize:fit:1024/1*OohqW5DGh9CQS4hLY5FXzA.png" },
-        { name: 'Shirt', description: 'Cotton shirt', price: 20.99, categoryId: categories[1].id, imageurl: "https://miro.medium.com/v2/resize:fit:1024/1*OohqW5DGh9CQS4hLY5FXzA.png" },
-        { name: 'Action Figure', description: 'Superhero action figure', price: 15.99, categoryId: categories[2].id, imageurl: "https://miro.medium.com/v2/resize:fit:1024/1*OohqW5DGh9CQS4hLY5FXzA.png" }
-    ]);
+    // Inserting Inventory
+    await models.Inventory.create({ stock: 10, productId: product1.id });
+    await models.Inventory.create({ stock: 50, productId: product2.id });
+    await models.Inventory.create({ stock: 30, productId: product3.id });
 
-    const inventories = await Inventory.bulkCreate([
-        { stock: 10, productId: products[0].id },
-        { stock: 50, productId: products[1].id },
-        { stock: 30, productId: products[2].id }
-    ]);
+    // Inserting Users
+    const user1 = await models.User.create({
+        firstName: 'John',
+        lastName: 'Doe',
+        password: 'password123',
+        email: 'johndoe@example.com',
+        phone_number: '1234567890'
+    });
 
-    const addresses = await Address.bulkCreate([
-        { zip_code: '10001', street_name: '1st Street', street_number: '10A', city: 'New York', userId: users[0].id },
-        { zip_code: '20002', street_name: '2nd Street', street_number: '20B', city: 'Los Angeles', userId: users[1].id },
-        { zip_code: '30003', street_name: '3rd Street', street_number: '30C', city: 'Chicago', userId: users[2].id }
-    ]);
+    // Inserting Address for User
+    await models.Address.create({
+        zip_code: '12345',
+        street_name: 'Main St',
+        street_number: '10A',
+        city: 'SampleCity',
+        userId: user1.id
+    });
 
-    const payments = await Payment.bulkCreate([
-        { total_price: 1520.49, userId: users[0].id },
-        { total_price: 36.98, userId: users[1].id },
-        { total_price: 45.99, userId: users[2].id }
-    ]);
+    // Inserting Order Status
+    const status1 = await models.OrderStatus.create({ name: 'Order received' });
 
-    const orders = await Order.bulkCreate([
-        { date: new Date(), orderStatusId: orderStatuses[0].id, paymentId: payments[0].id, userId: users[0].id },
-        { date: new Date(), orderStatusId: orderStatuses[1].id, paymentId: payments[1].id, userId: users[1].id },
-        { date: new Date(), orderStatusId: orderStatuses[2].id, paymentId: payments[2].id, userId: users[2].id }
-    ]);
+    // Inserting Payment
+    const payment1 = await models.Payment.create({ total_price: 1525, userId: user1.id });
 
-    const orderLineItems = await OrderLineItem.bulkCreate([
-        { orderId: orders[0].id, productId: products[0].id },
-        { orderId: orders[1].id, productId: products[1].id },
-        { orderId: orders[2].id, productId: products[2].id }
-    ]);
+    // Inserting Order
+    const order1 = await models.Order.create({
+        date: new Date(),
+        userId: user1.id,
+        orderStatusId: status1.id,
+        paymentId: payment1.id
+    });
 
-    const paymentDetails = await PaymentDetail.bulkCreate([
-        { card_number: '4111111111111111', transaction_number: 'TXN12345', paymentId: payments[0].id },
-        { card_number: '4222222222222222', transaction_number: 'TXN67890', paymentId: payments[1].id },
-        { card_number: '4333333333333333', transaction_number: 'TXN11223', paymentId: payments[2].id }
-    ]);
+    // Inserting Order Line Items
+    await models.OrderLineItem.create({ orderId: order1.id, productId: product1.id });
+    await models.OrderLineItem.create({ orderId: order1.id, productId: product2.id });
 
-    // Relationships
-    // Category
-    Category.hasMany(Product, { foreignKey: 'categoryId' });
+    // Inserting Payment Details
+    await models.PaymentDetail.create({
+        card_number: '1234123412341234',
+        transaction_number: 'tran_00001',
+        paymentId: payment1.id
+    });
 
-// Product
-    Product.belongsTo(Category, { foreignKey: 'categoryId' });
-    Product.hasOne(Inventory, { foreignKey: 'productId' });
-    Product.hasMany(OrderLineItem, { foreignKey: 'productId' });
-
-// Inventory
-    Inventory.belongsTo(Product, { foreignKey: 'productId' });
-
-// User
-    User.hasOne(Address, { foreignKey: 'userId' });
-    User.hasMany(Order, { foreignKey: 'userId' });
-    User.hasMany(Payment, { foreignKey: 'userId' });
-
-// Address
-    Address.belongsTo(User, { foreignKey: 'userId' });
-
-// Order
-    Order.belongsTo(User, { foreignKey: 'userId' });
-    Order.belongsTo(OrderStatus, { foreignKey: 'orderStatusId' });
-    Order.hasMany(OrderLineItem, { foreignKey: 'orderId' });
-    Order.hasMany(Payment, { foreignKey: 'orderId' });
-
-// OrderLineItem
-    OrderLineItem.belongsTo(Order, { foreignKey: 'orderId' });
-    OrderLineItem.belongsTo(Product, { foreignKey: 'productId' });
-
-// Payment
-    Payment.belongsTo(User, { foreignKey: 'userId' });
-    Payment.belongsTo(Order, { foreignKey: 'orderId' });
-    Payment.hasOne(PaymentDetail, { foreignKey: 'paymentId' });
-
-// PaymentDetail
-    PaymentDetail.belongsTo(Payment, { foreignKey: 'paymentId' });
-
-// OrderStatus
-    OrderStatus.hasMany(Order, { foreignKey: 'orderStatusId' });
-
-
-
-
-    console.log("Dummy data inserted!");
+    console.log('Dummy data inserted!');
 }
 
-insertDummyData()
-    .then(() => {
-        console.log("Finished inserting dummy data");
-        process.exit(0);
-    })
-    .catch(error => {
-        console.error("Error inserting dummy data:", error);
-        process.exit(1);
-    });
+async function main() {
+    await sequelize.sync({ force: true });
+    await insertDummyData();
+    process.exit();  // This will ensure the script exits after all operations
+}
+
+main().catch(error => {
+    console.error("Error occurred:", error);
+    process.exit(1);  // Exit with an error code
+});
--- models.js
+++ models.js
@@ -1,5 +1,4 @@
 import {Sequelize, Model, DataTypes} from "sequelize";
-import fs from "fs";
 
 const sequelize = new Sequelize('sql_store', 'admin1', 'Password123', {
     host: 'sql-store-kea-sd23.mysql.database.azure.com',
@@ -13,29 +12,48 @@ const sequelize = new Sequelize('sql_store', 'admin1', 'Password123', {
     }
 });
 
+// Category
 class Category extends Model {}
 Category.init({
     name: DataTypes.STRING(50)
 }, { sequelize, modelName: 'category' });
 
+// OrderStatus
 class OrderStatus extends Model {}
 OrderStatus.init({
     name: DataTypes.ENUM('Order received', 'In Progress', 'Order delivered')
 }, { sequelize, modelName: 'order_status' });
 
+// Product
 class Product extends Model {}
 Product.init({
     name: DataTypes.STRING(50),
     description: DataTypes.STRING(200),
     price: DataTypes.DECIMAL,
-    imageurl: DataTypes.STRING(255)
+    imageurl: DataTypes.STRING(255),
+    categoryId: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: Category,
+            key: 'id'
+        }
+    }
 }, { sequelize, modelName: 'product' });
 
+// Inventory
 class Inventory extends Model {}
 Inventory.init({
-    stock: DataTypes.SMALLINT
+    stock: DataTypes.SMALLINT,
+    productId: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: Product,
+            key: 'id'
+        }
+    }
 }, { sequelize, modelName: 'inventory' });
 
+// User
 class User extends Model {}
 User.init({
     firstName: DataTypes.STRING(40),
@@ -45,72 +63,134 @@ User.init({
     phone_number: DataTypes.STRING(100)
 }, { sequelize, modelName: 'user' });
 
+// Address
 class Address extends Model {}
 Address.init({
     zip_code: DataTypes.STRING(40),
     street_name: DataTypes.STRING(40),
     street_number: DataTypes.STRING(40),
-    city: DataTypes.STRING(40)
+    city: DataTypes.STRING(40),
+    userId: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: User,
+            key: 'id'
+        }
+    }
 }, { sequelize, modelName: 'address' });
 
+// Payment
 class Payment extends Model {}
 Payment.init({
-    total_price: DataTypes.DECIMAL
+    total_price: DataTypes.DECIMAL,
+    userId: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: User,
+            key: 'id'
+        }
+    }
 }, { sequelize, modelName: 'payment' });
 
+// Order
 class Order extends Model {}
 Order.init({
-    date: DataTypes.DATE
+    date: DataTypes.DATE,
+    userId: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: User,
+            key: 'id'
+        }
+    },
+    orderStatusId: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: OrderStatus,
+            key: 'id'
+        }
+    },
+    paymentId: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: Payment,
+            key: 'id'
+        }
+    }
 }, { sequelize, modelName: 'order' });
 
+// OrderLineItem
 class OrderLineItem extends Model {}
-OrderLineItem.init({}, { sequelize, modelName: 'order_line_item' });
+OrderLineItem.init({
+    orderId: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: Order,
+            key: 'id'
+        }
+    },
+    productId: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: Product,
+            key: 'id'
+        }
+    }
+}, { sequelize, modelName: 'order_line_item' });
 
+// PaymentDetail
 class PaymentDetail extends Model {}
 PaymentDetail.init({
     card_number: DataTypes.STRING(150),
-    transaction_number: DataTypes.STRING(150)
+    transaction_number: DataTypes.STRING(150),
+    paymentId: {
+        type: DataTypes.INTEGER,
+        references: {
+            model: Payment,
+            key: 'id'
+        }
+    }
 }, { sequelize, modelName: 'payment_detail' });
 
-// Category
+// Category and Product
+Product.belongsTo(Category, { foreignKey: 'categoryId', as: 'category' });
 Category.hasMany(Product, { foreignKey: 'categoryId' });
 
-// Product
-Product.belongsTo(Category, { foreignKey: 'categoryId' });
-Product.hasOne(Inventory, { foreignKey: 'productId' });
-Product.hasMany(OrderLineItem, { foreignKey: 'productId' });
-
-// Inventory
+// Product and Inventory
+Product.hasOne(Inventory, { foreignKey: 'productId', as: 'inventoryDetails' });
 Inventory.belongsTo(Product, { foreignKey: 'productId' });
 
-// User
-User.hasOne(Address, { foreignKey: 'userId' });
-User.hasMany(Order, { foreignKey: 'userId' });
-User.hasMany(Payment, { foreignKey: 'userId' });
-
-// Address
+// User and Address
+User.hasOne(Address, { foreignKey: 'userId', as: 'addresses' });
 Address.belongsTo(User, { foreignKey: 'userId' });
 
-// Order
-Order.belongsTo(User, { foreignKey: 'userId' });
-Order.belongsTo(OrderStatus, { foreignKey: 'orderStatusId' });
-Order.hasMany(OrderLineItem, { foreignKey: 'orderId' });
-Order.hasMany(Payment, { foreignKey: 'orderId' });
+// User and Payment
+User.hasMany(Payment, { foreignKey: 'userId', as: 'payments' });
+Payment.belongsTo(User, { foreignKey: 'userId' });
 
-// OrderLineItem
-OrderLineItem.belongsTo(Order, { foreignKey: 'orderId' });
+// User and Order
+User.hasMany(Order, { foreignKey: 'userId', as: 'orders' });
+Order.belongsTo(User, { foreignKey: 'userId' });
+
+// OrderStatus and Order
+Order.belongsTo(OrderStatus, { foreignKey: 'orderStatusId', as: 'statusDetails' });
+OrderStatus.hasMany(Order, { foreignKey: 'orderStatusId' });
+
+// Payment and Order
+//Order.belongsTo(Payment, { foreignKey: 'paymentId', as: 'paymentDetails' });
+Payment.hasOne(Order, { foreignKey: 'paymentId' });
+
+// Product and OrderLineItem
+Product.hasMany(OrderLineItem, { foreignKey: 'productId', as: 'lineItems' });
 OrderLineItem.belongsTo(Product, { foreignKey: 'productId' });
 
-// Payment
-Payment.belongsTo(User, { foreignKey: 'userId' });
-Payment.belongsTo(Order, { foreignKey: 'orderId' });
-Payment.hasOne(PaymentDetail, { foreignKey: 'paymentId' });
+// Order and OrderLineItem
+Order.hasMany(OrderLineItem, { foreignKey: 'orderId', as: 'lineItems' });
+OrderLineItem.belongsTo(Order, { foreignKey: 'orderId' });
 
-// PaymentDetail
+// Payment and PaymentDetail
+Payment.hasMany(PaymentDetail, { foreignKey: 'paymentId', as: 'paymentDetails' });
 PaymentDetail.belongsTo(Payment, { foreignKey: 'paymentId' });
-
-// OrderStatus
-OrderStatus.hasMany(Order, { foreignKey: 'orderStatusId' });
 
 
 // Synchronize the models with the database
@@ -128,4 +208,3 @@ export default {
     OrderLineItem,
     PaymentDetail
 }
-
