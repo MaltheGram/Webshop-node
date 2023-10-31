e589e542af17644bca40143d264645f65386d49f
Malthe <malthegram22@gmail.com>
2023-10-31 15:29:09 +0100

    order and user service done

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 import express from 'express';
 import swaggerUi from 'swagger-ui-express';
+import swaggerDocs from "./docs/swagger.json" assert {type: "json"}
+import userRouter from "./routers/userRouter.js";
+import orderRouter from "./routers/orderRouter.js"
 
 
 const app = express();
@@ -9,13 +12,14 @@ app.get("/", (req, res) => {
     res.send({message: "Please go to /api-docs"})
 })
 
+app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
 
-import userRouter from "./routers/userRouter.js";
 app.use(userRouter);
+app.use(orderRouter)
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(""));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.listen(PORT, () => {
     console.log(`Server started on http://localhost:${PORT}`);
--- service/UserService.js
+++ service/UserService.js
@@ -1,14 +1,62 @@
-import model from "models.js"
+import model from "../models.js"
 
 class UserService {
 
 
     constructor() {
     }
-     static create = (params) => {
-        model.User.create(params)
+
+    static getAll = async () => {
+        return await model.User.findAll()
+    }
+    static getById = async (id) => {
+        return await model.User.findByPk(id)
+    }
+    static create = async (params) => {
+        // Start a transaction
+        const transaction = await model.sequelize.transaction();
+
+        try {
+            const user = await model.User.create(params, {transaction})
+            const userId = user.id;
+
+            const addressParams = {
+                ...params.Address,
+                userId: userId
+            };
+
+            await model.Address.create(addressParams, {transaction})
+
+            await transaction.commit();
+
+        } catch (error) {
+            await transaction.rollback();
+            throw error
+        }
+    }
+    static update = async (userId, updateData) => {
+        try {
+            const user = await model.User.findByPk(userId);
+            if (!user) {
+                throw new Error('User not found');
+            }
+
+            return user.update(updateData);
+
+        } catch (error) {
+            throw error;
+        }
     }
 
+    static delete = async (id) => {
+        try {
+            return await model.User.destroy({where: {id: id}})
+
+        } catch (err) {
+            throw err
+        }
+
+    }
 }
 
 
