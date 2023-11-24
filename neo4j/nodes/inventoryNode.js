d5f76fa66a0ce3b682cdc33b43404562a048bbf9
Malthe <malthegram22@gmail.com>
2023-11-24 10:27:47 +0100

    nodes for neo4j

---
+++ neo4j/nodes/inventoryNode.js
@@ -0,0 +1,24 @@
+import { neo4j as neo4jDriver } from "../database.js";
+
+const createInventoryNode = async (inventory) => {
+  const session = neo4jDriver.seession();
+
+  try {
+    const result = await session.run(
+      `
+            CREATE (i:Inventory {
+                id: $id, 
+                quaintityInStock: $quantityInStock
+                 }) RETURN i`,
+      {
+        id: inventory.id,
+        quantityInStock: inventory.quantityInStock,
+      },
+    );
+    return result.records;
+  } catch (error) {
+    console.log(error);
+  } finally {
+    await session.close();
+  }
+};
